935d828a6641a741176a186ce0f6d551d2c3b93b
Richard Porteous <rich.porteous@gmail.com>
2022-01-22 16:20:47 -0700

    Step 2 - Copy Calculate functions. Create controller variable to match the winforms textboxes.
    Use properties for new variables (or stay consistant and use functions).
    Don't wire up anything, compile and test.

--- IntervalController.cs
+++ IntervalController.cs
@@ -11,16 +11,95 @@ namespace interval_refactor_project
         private IntervalModel model;
         private IntervalView view;
 
+        private string _end = "0";
+        private string _start = "0";
+        private string _length = "0";
+
+        public string end
+        {
+            get => _end; 
+            set
+            {
+                _end = value;
+                SetEndChanged();
+                NotifyObservers();
+            }
+        }
+
+        public string start { 
+            get => _start;
+            set
+            {
+                _start = value;
+                SetStartChanged();
+                NotifyObservers();
+
+            }
+        }
+
+        public string length { 
+            get => _length;
+            set { 
+                _length = value;
+                SetLengthChanged();
+                NotifyObservers();
+            }
+        }
+
         public IntervalController(IntervalView view)
         {
             this.model = new IntervalModel();
             this.view = view;
         }
 
-        private void SetChanged() { }
-        private void NotifyObservers() 
+
+        private void SetStartChanged()
+        {
+            CalculateLength();
+        }
+        private void SetEndChanged()
+        {
+            CalculateLength();
+        }
+        private void SetLengthChanged()
+        {
+            CalculateEnd();
+        }
+
+        private void NotifyObservers()
         {
             view.Update();
+        }
+
+
+        private void CalculateLength()
+        {
+            try
+            {
+                int start = int.Parse(_start);
+                int end = int.Parse(_end);
+                int length = end - start;
+                _length = length.ToString();
+            }
+            catch (Exception)
+            {
+                throw new FormatException("Unexpected Number Format Error");
+            }
+        }
+
+        private void CalculateEnd()
+        {
+            try
+            {
+                int start = int.Parse(_start);
+                int length = int.Parse(_length);
+                int end = length + start;
+                _end = end.ToString();
+            }
+            catch (Exception)
+            {
+                throw new FormatException("Unexpected Number Format Error");
+            }
         }
     }
 
