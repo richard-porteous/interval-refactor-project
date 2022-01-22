6f2eb7e14f78f15054e055a239f6c797e2521647
Richard Porteous <rich.porteous@gmail.com>
2022-01-22 15:31:12 -0700

    Step 0 - Original WinForms Interval Application
    One winforms view - no data - in this simple example

---
+++ IntervalView.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace interval_refactor_project
+{
+    partial class IntervalView
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._startField = new System.Windows.Forms.TextBox();
+            this._endField = new System.Windows.Forms.TextBox();
+            this._lengthField = new System.Windows.Forms.TextBox();
+            this.startLBL = new System.Windows.Forms.Label();
+            this.endLBL = new System.Windows.Forms.Label();
+            this.lengthLBL = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // _startField
+            // 
+            this._startField.Location = new System.Drawing.Point(94, 80);
+            this._startField.Name = "_startField";
+            this._startField.Size = new System.Drawing.Size(133, 23);
+            this._startField.TabIndex = 0;
+            this._startField.Text = "0";
+            this._startField.LostFocus += new System.EventHandler(this.StartField_LostFocus);
+            // 
+            // _endField
+            // 
+            this._endField.Location = new System.Drawing.Point(94, 153);
+            this._endField.Name = "_endField";
+            this._endField.Size = new System.Drawing.Size(133, 23);
+            this._endField.TabIndex = 1;
+            this._endField.Text = "0";
+            this._endField.LostFocus += new System.EventHandler(this.EndField_LostFocus);
+            // 
+            // _lengthField
+            // 
+            this._lengthField.Location = new System.Drawing.Point(94, 230);
+            this._lengthField.Name = "_lengthField";
+            this._lengthField.Size = new System.Drawing.Size(133, 23);
+            this._lengthField.TabIndex = 2;
+            this._lengthField.Text = "0";
+            this._lengthField.LostFocus += new System.EventHandler(this.LengthField_LostFocus);
+            // 
+            // startLBL
+            // 
+            this.startLBL.AutoSize = true;
+            this.startLBL.Location = new System.Drawing.Point(22, 83);
+            this.startLBL.Name = "startLBL";
+            this.startLBL.Size = new System.Drawing.Size(31, 15);
+            this.startLBL.TabIndex = 3;
+            this.startLBL.Text = "Start";
+            // 
+            // endLBL
+            // 
+            this.endLBL.AutoSize = true;
+            this.endLBL.Location = new System.Drawing.Point(22, 156);
+            this.endLBL.Name = "endLBL";
+            this.endLBL.Size = new System.Drawing.Size(27, 15);
+            this.endLBL.TabIndex = 4;
+            this.endLBL.Text = "End";
+            // 
+            // lengthLBL
+            // 
+            this.lengthLBL.AutoSize = true;
+            this.lengthLBL.Location = new System.Drawing.Point(22, 233);
+            this.lengthLBL.Name = "lengthLBL";
+            this.lengthLBL.Size = new System.Drawing.Size(44, 15);
+            this.lengthLBL.TabIndex = 5;
+            this.lengthLBL.Text = "Length";
+            // 
+            // IntervalWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 450);
+            this.Controls.Add(this.lengthLBL);
+            this.Controls.Add(this.endLBL);
+            this.Controls.Add(this.startLBL);
+            this.Controls.Add(this._lengthField);
+            this.Controls.Add(this._endField);
+            this.Controls.Add(this._startField);
+            this.Name = "IntervalWindow";
+            this.Text = "Interval Window";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox _startField;
+        private System.Windows.Forms.TextBox _endField;
+        private System.Windows.Forms.TextBox _lengthField;
+        private System.Windows.Forms.Label startLBL;
+        private System.Windows.Forms.Label endLBL;
+        private System.Windows.Forms.Label lengthLBL;
+    }
+}
+
---
+++ IntervalView.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace interval_refactor_project
+{
+    public partial class IntervalView : Form
+    {
+        public IntervalView()
+        {
+            InitializeComponent();
+        }
+
+
+        private void StartField_LostFocus(object sender, EventArgs e)
+        {
+            if (!int.TryParse(_startField.Text.ToString(), out int result))
+            {
+                _startField.Text = "0";
+            }
+            //var foo = result;
+            CalculateLength();
+        }
+
+        private void EndField_LostFocus(object sender, EventArgs e)
+        {
+            SetEnd(_endField.Text); //catches a direct set of _endField
+                                    //and forces the value through the method
+                                    //becomes obvious when using the domain model
+            if (!int.TryParse(GetEnd(), out int result))
+            {
+                SetEnd("0");
+            }
+            //var foo = result;
+            CalculateLength();
+        }
+
+        private void LengthField_LostFocus(object sender, EventArgs e)
+        {
+            if (!int.TryParse(_lengthField.Text.ToString(), out int result))
+            {
+                _lengthField.Text = "0";
+            }
+            //var foo = result;
+            CalculateEnd();
+        }
+
+        private string GetEnd()
+        {
+            return _endField.Text;
+        }
+        private void SetEnd(string arg)
+        {
+            _endField.Text = arg;
+        }
+
+        private void CalculateLength() 
+        {
+            try
+            {
+                int start = int.Parse(_startField.Text);
+                int end = int.Parse(GetEnd());
+                int length = end - start;
+                _lengthField.Text = length.ToString();
+            }
+            catch (Exception)
+            {
+                throw new FormatException("Unexpected Number Format Error");
+            }
+        }
+
+        private void CalculateEnd() 
+        {
+            try
+            {
+                int start = int.Parse(_startField.Text);
+                int length = int.Parse(_lengthField.Text);
+                int end = length + start;
+                SetEnd(end.ToString());
+            }
+            catch (Exception)
+            {
+                throw new FormatException("Unexpected Number Format Error");
+            }
+        }
+    }
+}
